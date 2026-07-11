035fc85c776c8dc54bbee3abedf0d5d961c5fa61
dailymarketonline1 <yelahi03@gmail.com>
2026-07-11 09:31:48 +0500

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+session_destroy();
+setcookie('user_id', '', time() - 3600, '/');
+setcookie('user_type', '', time() - 3600, '/');
+header('Location: login.php');
+exit;
+?>
